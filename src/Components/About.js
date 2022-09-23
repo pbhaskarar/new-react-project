1f58c85fa77a6fc8b2cdb4e4ed9623f49dbf6c91
Bhaskar p <pbhaskar870@gmail.com>
2022-09-23 15:01:06 +0530

    adda home page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import '../App.css';
 import Home from './Home';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
@@ -14,12 +15,13 @@ const About = (props) => {
     setData({...data,[e.target.name]:[e.target.value]})
   }
   return (
-    <div>
+    <div className='subbmit-form'>
       <center>
+        <h1>Simple Submit form </h1>
       <form>
-            <input type ="text" name='username' value={username} onChange={changeHandler} /> <br/>
-            <input type ="password" name='password' value={password} onChange={changeHandler} /><br/>
-            <input type="submit" name='submit' onClick={()=>{props.onSubmit("Home")}} />
+            <input type ="text" name='username' value={username} onChange={changeHandler}  placeholder="username"/> <br/><br></br>
+            <input type ="password" name='password' value={password} onChange={changeHandler} placeholder="password" /><br/><br></br>
+            <input type="submit" name='submit'placeholder='submit' onClick={()=>{props.onSubmit("Home")}} />
           </form>
       </center>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap';
 
 const Home = () => {
+  const[ form,setForm] = useState({
+    username : "",
+    email : "",
+    password : "",
+    confirmPassword :"",
+  })
+  const{username,email,password,confirmPassword} = form;
+  const changeHandler = e =>{
+    FormData({...form,[e.target.name]:[e.target.value]})
+  }
+  const submitHandler = e=>{
+    e.preventDefault();
+    if(password===confirmPassword){
+      console.log(form);
+    }
+    else{
+      console.log("password not matched");
+    }
+  }
+
   return (
     <div>
     <center>
       <h1>Home</h1>
+      <form onSubmit={submitHandler}>
+      <input type="text" name='username' value={username} placeholder="username" onChange={changeHandler}/> <br></br>
+      <input type="email" name='email' value={email}  placeholder="email" onChange={changeHandler}/><br></br>
+      <input type="password" name='password' value={password} placeholder="password" onChange={changeHandler}/><br></br>
+      <input type="passwoed" name='confirmPassword' value={confirmPassword} placeholder="password" onChange={changeHandler} /><br></br>
+      <input type="submit" name='submit' placeholder='submit' />
+      </form>
     </center>  
     </div>
   )
 }
 
-export default Home
+export default Home;
