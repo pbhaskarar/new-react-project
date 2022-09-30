8f8c01e2799382461ec8f138a644b8409fd4cc8e
Bhaskar p <pbhaskar870@gmail.com>
2022-09-30 13:52:43 +0530

    api  used in react

--- src/Components/InputWithIcon.js
+++ src/Components/InputWithIcon.js
@@ -1,8 +1,22 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
 
 const InputWithIcon = () => {
+  const[data, setData] = useState([]);
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/albums").then(response=>setData(response.data))
+  },[])
   return (
-    <div>InputWithIcon</div>
+    <>
+    <center>
+      <h1>api request using axios</h1>
+    {
+      data.map(item=><li key={item.id}>{item.title}</li>)
+    }
+    </center>
+    </>
   )
 }
 
